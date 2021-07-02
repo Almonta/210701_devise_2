bd444dc06e64a8b9d16d48168d31bd4734e2d340
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-02 09:25:27 +0900

    rails_adminをインストールした

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'devise'
+gem 'devise-i18n'
+gem 'rails_admin', '~> 2.0'
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'blog#index'
+  root 'blogs#index'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
